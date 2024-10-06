e831a329d4a11c3e98f5f5d9a97ef880ca798190
FelopateerEkladious <felopateerekladiuos@gmail.com>
2024-10-06 23:42:45 +0200

    Delete aws_IaC/s3 directory

--- aws_IaC/s3/s3_bucket_policy.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket_policy" "tfer--my-food-data-bucket" {
-  bucket = "my-food-data-bucket"
-  policy = "{\"Statement\":[{\"Action\":\"s3:GetObject\",\"Effect\":\"Allow\",\"Principal\":\"*\",\"Resource\":\"arn:aws:s3:::my-food-data-bucket/*\"}],\"Version\":\"2012-10-17\"}"
-}
